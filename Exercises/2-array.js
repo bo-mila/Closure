0465bfa7bb89677c180c05fbc30466fd8ba3e849
Mila Bozhevskaya <mi.bozhev@gmail.com>
2021-07-23 14:09:44 +0300

    done

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,22 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const data = [];
+  const get = (i) => data[i];
+  get.push = (x) => data.push(x);
+  get.pop = () => data.pop();
+  return get;
+};
+
+// if (typeof i !== 'number') {
+//   return {
+//     push(x) {
+//       data.push(x);
+//     },
+//     pop() {
+//       data.pop();
+//     }
+//   };
+// }
 
 module.exports = { array };
